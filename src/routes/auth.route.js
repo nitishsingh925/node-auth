6654ff4b839975043c99b997b1f10802cac7cbdc
nitish singh <nitishsingh925@gmail.com>
2024-05-03 19:54:15 +0530

    add zod validation

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { signin, signup } from "../controllers/auth.controller.js";
+import { validation } from "../middlewares/validation-middleware.js";
+import { signupValidator } from "../validation/signup.validation.js";
+import { signinValidator } from "../validation/signin.vailidation.js";
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/signup", validation(signupValidator), signup);
+router.post("/signin", validation(signinValidator), signin);
 export default router;
---
+++ src/validation/signin.vailidation.js
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const signinValidator = z.object({
+  // username
+  username: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(3, { message: "Name must be at least 3 characters" })
+    .max(50, { message: "Name must be at most 50 characters" })
+    .regex(/^[a-zA-Z0-9]+$/)
+    .optional(),
+  // email
+  email: z
+    .string({ required_error: "email is required" })
+    .trim()
+    .email()
+    .max(100)
+    .optional(),
+  // password
+  password: z
+    .string({ required_error: "password  is required" })
+    .min(6)
+    .max(100),
+});

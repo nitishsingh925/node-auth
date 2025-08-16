d86052733c580e305d37322b2e46fbdd67558503
Nitish Singh <nitishsingh925@gmail.com>
2025-08-16 16:09:34 +0530

    fallback round add

--- src/app.js
+++ src/app.js
@@ -8,4 +8,9 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 
+// Fallback route
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 export { app };
